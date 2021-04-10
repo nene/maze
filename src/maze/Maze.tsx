10fc89354f640dec0323f482d65c9f2c78025f1e
Rene Saarsoo <nene@triin.net>
2021-04-10 12:49:33 +0300

    Add styled comps

--- src/maze/Maze.tsx
+++ src/maze/Maze.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 type MazeCell = number;
 
@@ -10,8 +11,15 @@ const data: MazeCell[][] = [
   [1, 2, 3, 4, 5],
 ];
 
+const C = styled.span`
+  display: inline-block;
+  width: 25px;
+  height: 25px;
+  border: 1px solid #3e7485;
+`;
+
 const Cell: React.FC<{cell: MazeCell}> = ({cell}) => {
-  return <span>{cell}</span>
+  return <C>{cell}</C>
 }
 
 export const Maze: React.FC<{}> = () => {
@@ -20,4 +28,4 @@ export const Maze: React.FC<{}> = () => {
       { data.map(row => <div>{ row.map(cell => <Cell cell={cell} />) }</div>) }
     </div>
   );
-}
+};
