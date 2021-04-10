6ac40cb7a4c065092c9803e0165bfff77b82036d
Rene Saarsoo <nene@triin.net>
2021-04-10 19:56:27 +0300

    Make algorithm recursive

--- src/maze/createMaze.ts
+++ src/maze/createMaze.ts
@@ -1,6 +1,6 @@
 import seedrandom from "seedrandom";
-import { Coord, Dir, Maze, MazeCell } from "./Maze";
-import { availableDirs, cellAt, cellPlusDir, cellPlusMarker, coordPlusDir, emptyCell, emptyMaze, isCellEmpty, mazeSize, oppositeDir, setCellAt } from "./maze-utils";
+import { Coord, Dir, Maze } from "./Maze";
+import { availableDirs, cellAt, cellPlusDir, cellPlusMarker, emptyMaze, oppositeDir, setCellAt } from "./maze-utils";
 
 const WIDTH = 40;
 const HEIGHT = 40;
@@ -20,27 +20,35 @@ const nextCell = (p: Coord, maze: Maze): {coord: Coord, dir: Dir} | undefined =>
   return randomFrom(directions);
 }
 
+const connectCells = (p1: Coord, p2: Coord, dir: Dir, maze: Maze): Maze => {
+  maze = setCellAt(p1, cellPlusDir(cellAt(p1, maze), dir), maze);
+  maze = setCellAt(p2, cellPlusDir(cellAt(p2, maze), oppositeDir(dir)), maze);
+  return maze;
+}
+
+const drawPath = (p: Coord, maze: Maze): Maze => {
+  const {coord: p2, dir} = nextCell(p, maze) || {};
+
+  if (!p2 || !dir) {
+    // mark last cell
+    maze = setCellAt(p, cellPlusMarker(cellAt(p, maze), true), maze);
+    return maze;
+  }
+
+  maze = connectCells(p, p2, dir, maze);
+  maze = drawPath(p2, maze);
+  return maze;
+}
+
 export function createMaze(): Maze {
   let maze = emptyMaze(WIDTH, HEIGHT);
 
   let p: Coord = {x: 20, y: 20};
+
   // mark first cell
-  let cell: MazeCell | undefined = cellPlusMarker(cellAt(p, maze) || emptyCell(), true);
-
-  while (p) {
-    const {coord: p2, dir} = nextCell(p, maze) || {};
-    if (!p2 || !dir) {
-      break;
-    }
-
-    maze = setCellAt(p, cellPlusDir(cell || emptyCell(), dir), maze);
-    maze = setCellAt(p2, cellPlusDir(emptyCell(), oppositeDir(dir)), maze);
-    p = p2;
-    cell = cellAt(p2, maze);
-  }
-
-  // mark last cell
-  maze = setCellAt(p, cellPlusMarker(cell || emptyCell(), true), maze);
+  maze = setCellAt(p, cellPlusMarker(cellAt(p, maze), true), maze);
+  // draw line
+  maze = drawPath(p, maze);
 
   return maze;
 }
