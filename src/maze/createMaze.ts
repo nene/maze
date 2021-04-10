0d0a5342cf7aa2e4e2fa790ba4446f742408a56f
Rene Saarsoo <nene@triin.net>
2021-04-10 14:15:41 +0300

    Some actual maze path generation :D

--- src/maze/createMaze.ts
+++ src/maze/createMaze.ts
@@ -1,4 +1,5 @@
-import { range } from "ramda";
+import { RSA_PKCS1_OAEP_PADDING } from "node:constants";
+import { clone, range } from "ramda";
 
 export type MazeCell = {
   top: boolean;
@@ -7,21 +8,93 @@ export type MazeCell = {
   left: boolean;
 };
 
-const width = 10
-const height = 10
+type FullMaze = MazeCell[][];
+type MaybeMaze = (MazeCell | undefined)[][];
+
+type Dir = keyof MazeCell;
+
+type Coord = { x: number, y: number };
+
+const WIDTH = 10
+const HEIGHT = 10
 
 function randomFrom<T>(arr: T[]): T {
   const index = Math.floor(Math.random() * arr.length);
   return arr[index];
 }
 
-export function createMaze(): MazeCell[][] {
+const emptyMaze = (width: number, height: number): undefined[][] => {
   return range(0, height).map(
-    () => range(0, width).map(() => ({
-      top: randomFrom([true, false]),
-      right: randomFrom([true, false]),
-      bottom: randomFrom([true, false]),
-      left: randomFrom([true, false]),
-    }))
+    () => range(0, width).map(() => undefined)
   );
+}
+
+const emptyCell = (): MazeCell => ({ top: false, bottom: false, left: false, right: false });
+
+const fillEmptySlots = (maze: MaybeMaze): FullMaze => {
+  return maze.map(row => row.map(c => c || emptyCell()));
+}
+
+const coordPlusDir = ({x, y}: Coord, size: Coord, dir: Dir): Coord | undefined => {
+  switch (dir) {
+    case 'left': return x > 0 ? { x: x - 1, y } : undefined;
+    case 'right': return x < size.x - 1 ? { x: x + 1, y } : undefined;
+    case 'top': return y > 0 ? { x, y: y - 1 } : undefined;
+    case 'bottom': return y < size.y - 1 ? { x, y: y + 1 } : undefined;
+  }
+}
+
+const oppositeDir = (dir: Dir): Dir => {
+  switch (dir) {
+    case 'left': return 'right';
+    case 'right': return 'left';
+    case 'top': return 'bottom';
+    case 'bottom': return 'top';
+  }
+}
+
+const cellAt = (p: Coord, maze: MaybeMaze): MazeCell | undefined => maze[p.y][p.x];
+
+const cellPlusDir = (cell: MazeCell, dir: Dir): MazeCell => {
+  const c = clone(cell);
+  c[dir] = true;
+  return c;
+}
+
+const mazeSize = <T>(maze: T[][]): Coord => ({
+  y: maze.length,
+  x: maze[0].length,
+});
+
+const nextCell = (p: Coord, maze: MaybeMaze): {p2: Coord, dir: Dir} | undefined => {
+  let directions: Dir[] = ['left', 'right', 'top', 'bottom'];
+  while (directions.length > 0) {
+    const dir = randomFrom(directions);
+    const p2 = coordPlusDir(p, mazeSize(maze), dir);
+    if (p2 && !cellAt(p2, maze)) {
+      return {p2, dir};
+    }
+    directions = directions.filter(d => d !== dir);
+  }
+  return undefined;
+}
+
+export function createMaze(): FullMaze {
+  const maze: MaybeMaze = emptyMaze(WIDTH, HEIGHT);
+
+  let p: Coord = {x: 5, y: 5};
+  let cell = cellAt(p, maze);
+  while (p) {
+    const {p2, dir} = nextCell(p, maze) || {};
+    if (!p2 || !dir) {
+      break;
+    }
+
+    maze[p.y][p.x] = cellPlusDir(cell || emptyCell(), dir);
+    maze[p2.y][p2.x] = cellPlusDir(emptyCell(), oppositeDir(dir));
+    p = p2;
+    cell = cellAt(p2, maze);
+  }
+
+  return fillEmptySlots(maze);
 }
