1b1443fc0c8defda5e6d4b70b07b9367cd10fff4
Rene Saarsoo <nene@triin.net>
2021-04-10 13:15:53 +0300

    Rendering of maze lines

--- src/maze/Maze.tsx
+++ src/maze/Maze.tsx
@@ -2,30 +2,94 @@ import React from "react";
 import styled from "styled-components";
 import { range } from "ramda";
 
-type MazeCell = number;
+type MazeCell = {
+  top: boolean;
+  right: boolean;
+  bottom: boolean;
+  left: boolean;
+};
 
 const width = 10
 const height = 10
 
+function randomFrom<T>(arr: T[]): T {
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+
 const data: MazeCell[][] = range(0, height).map(
-  () => range(0, width)
+  () => range(0, width).map(() => ({
+    top: randomFrom([true, false]),
+    right: randomFrom([true, false]),
+    bottom: randomFrom([true, false]),
+    left: randomFrom([true, false]),
+  }))
 );
 
-const C = styled.span`
-  display: inline-block;
-  width: 25px;
-  height: 25px;
-  border: 1px solid #3e7485;
+const RowWrap = styled.div`
+  display: flex;
 `;
 
-const Cell: React.FC<{cell: MazeCell}> = ({cell}) => {
-  return <C>{cell}</C>
+const CellWrap = styled.span`
+  display: block;
+  position: relative;
+  width: 16px;
+  height: 16px;
+  border: 1px solid #d0edf7;
+`;
+
+const Line = styled.span`
+  position: absolute;
+  background-color: #0a0422;
+`;
+
+const Top = styled(Line)`
+  top: 0;
+  left: 7px;
+  width: 2px;
+  height: 8px;
+`;
+
+const Bottom = styled(Line)`
+  bottom: 0;
+  left: 7px;
+  width: 2px;
+  height: 8px;
+`;
+
+const Left = styled(Line)`
+  top: 7px;
+  left: 0;
+  width: 8px;
+  height: 2px;
+`;
+
+const Right = styled(Line)`
+  top: 7px;
+  right: 0;
+  width: 8px;
+  height: 2px;
+`;
+
+const Cell: React.FC<MazeCell> = ({top, right, bottom, left}) => {
+  return (
+    <CellWrap>
+      {top ? <Top/> : undefined}
+      {bottom ? <Bottom/> : undefined}
+      {left ? <Left/> : undefined}
+      {right ? <Right/> : undefined}
+    </CellWrap>
+  );
 }
 
 export const Maze: React.FC<{}> = () => {
   return (
     <div>
-      { data.map(row => <div>{ row.map(cell => <Cell cell={cell} />) }</div>) }
+      { data.map((row, i) =>
+          <RowWrap key={i}>
+            { row.map((cell, j) => <Cell key={i*1000+j} {...cell} />) }
+          </RowWrap>
+      ) }
     </div>
   );
 };
